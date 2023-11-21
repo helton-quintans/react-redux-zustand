8c2c3f8ca7f7c834cca56b199c9859f6c52a4126
helton-quintans <helton.quit@gmail.com>
2023-11-20 22:06:56 -0300

    :sparkles: feat: Heade

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,3 +1,5 @@
+import { MessageCircle } from 'lucide-react'
+
 export function Player() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
@@ -10,7 +12,10 @@ export function Player() {
               Módulo "Desvendando o redux"
             </span>
           </div>
-          <button>Deixar feedback</button>
+          <button className='flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600'>
+            <MessageCircle className='w-4 h-4' />
+            Deixar feedback
+          </button>
         </div>
 
         <main>
