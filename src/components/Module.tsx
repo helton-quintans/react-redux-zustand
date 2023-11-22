d416aa7da3eef49bce578cb995cf01052e228e35
helton-quintans <helton.quit@gmail.com>
2023-11-22 09:41:28 -0300

    :sparkles: sidebar modules with colapseble

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,6 +1,8 @@
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from './Lessoson'
 
+import * as Collapsible from '@radix-ui/react-collapsible'
+
 interface ModuleProps {
   moduleIndex: number
   title: string
@@ -9,8 +11,8 @@ interface ModuleProps {
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className='group'>
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <span className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </span>
@@ -20,14 +22,15 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
           <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
 
-        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-      </button>
-
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title="Fundamentos do universo" duration="7:77" />
-        <Lesson title="Fundamentos do universo" duration="7:77" />
-        <Lesson title="Fundamentos do universo" duration="7:77" />
-      </nav>
-    </div>
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do universo" duration="7:77" />
+          <Lesson title="Fundamentos do universo" duration="7:77" />
+          <Lesson title="Fundamentos do universo" duration="7:77" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
