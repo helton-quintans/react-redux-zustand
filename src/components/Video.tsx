b4f20732e64639dbb05fa570fe3b0077ede01098
helton-quintans <helton.quit@gmail.com>
2024-06-27 17:18:45 -0300

    feat: add NextLesson logic

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 import ReactPlayer from 'react-player'
+import { useDispatch } from 'react-redux'
 import { useAppSelector } from '../store'
+import { nextLesson } from '../store/slices/player'
 
 export function Video() {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex} = state.palyer
 
@@ -10,12 +14,18 @@ export function Video() {
 
     return currentLesson
   })
+
+  function handlePlayNextLesson() {
+    dispatch(nextLesson())
+  }
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
+        playing
+        onEnded={handlePlayNextLesson}
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
